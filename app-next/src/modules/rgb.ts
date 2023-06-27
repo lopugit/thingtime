6068a00eb6618d0c1e74eb57490d568a3f509021
Nikolaj Frey <nikolaj@dotdev.com.au>
2023-06-27 17:57:48 +1000

    feat: main Deleted next app

--- app-next/src/modules/rgb.ts
+++
@@ -1,6 +0,0 @@
-import hexRgb from 'hex-rgb'
-
-export default (hex: string, alpha: number) => {
-  const rgb = hexRgb(hex)
-  return `rgba(${rgb.red}, ${rgb.green}, ${rgb.blue}, ${typeof alpha === 'number' ? alpha : rgb.alpha})`
-}
