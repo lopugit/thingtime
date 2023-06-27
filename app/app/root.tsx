993c88b3efd7fe681616f80a053e1915be69b4f9
Nikolaj Frey <nikolaj@dotdev.com.au>
2023-06-27 10:30:43 +1000

    feat: main

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -13,7 +13,7 @@ import { Analytics } from '@vercel/analytics/react'
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Thingtime | For Everything',
   viewport: 'width=device-width,initial-scale=1'
 })
 
