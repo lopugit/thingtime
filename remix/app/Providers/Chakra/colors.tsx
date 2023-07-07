2a8eb2297634c4d5b0a7567192f92a8ef4593c9f
Nikolaj Frey <nikolaj@dotdev.com.au>
2023-07-07 17:42:14 +1000

    feat: feature/rainbow-component Fixed a few things

--- remix/app/Providers/Chakra/colors.tsx
+++ remix/app/Providers/Chakra/colors.tsx
@@ -1,45 +1,46 @@
 const g = {
-  grey: '#F1F1F3',
+  grey: "#F1F1F3",
   greys: {
-    light: '#F1F1F3',
-    medium: '#E0E0E0',
-    dark: '#BDBDBD'
-  }
+    light: "#F1F1F3",
+    lightt: "#E7E6E8",
+    medium: "#E0E0E0",
+    dark: "#BDBDBD",
+  },
 }
 
 g.gray = g.grey
 g.grays = g.greys
 
 export const colors = {
-  white: '#FFFFFF',
+  white: "#FFFFFF",
   ...g,
-  black: '#000000',
+  black: "#000000",
   // all colors of the chakras
   chakras: {
-    root: '#C62828',
-    sacral: '#FF7043',
-    solarPlexus: '#FFEE58',
-    heart: '#66BB6A',
-    throat: '#42A5F5',
-    thirdEye: '#5C6BC0',
-    crown: '#AB47BC',
+    root: "#C62828",
+    sacral: "#FF7043",
+    solarPlexus: "#FFEE58",
+    heart: "#66BB6A",
+    throat: "#42A5F5",
+    thirdEye: "#5C6BC0",
+    crown: "#AB47BC",
 
-    red: '#C62828',
-    orange: '#FF7043',
-    yellow: '#FFEE58',
-    green: '#66BB6A',
-    blue: '#42A5F5',
-    indigo: '#5C6BC0',
-    violet: '#AB47BC'
+    red: "#C62828",
+    orange: "#FF7043",
+    yellow: "#FFEE58",
+    green: "#66BB6A",
+    blue: "#42A5F5",
+    indigo: "#5C6BC0",
+    violet: "#AB47BC",
   },
   // all colors of the rainbow
   rainbow: {
-    red: '#FF0000',
-    orange: '#FF7F00',
-    yellow: '#FFFF00',
-    green: '#00FF00',
-    blue: '#0000FF',
-    indigo: '#4B0082',
-    violet: '#8F00FF'
-  }
+    red: "#FF0000",
+    orange: "#FF7F00",
+    yellow: "#FFFF00",
+    green: "#00FF00",
+    blue: "#0000FF",
+    indigo: "#4B0082",
+    violet: "#8F00FF",
+  },
 }
--- remix/app/components/Nav/Nav.tsx
+++ remix/app/components/Nav/Nav.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Box, Flex } from '@chakra-ui/react'
-import { RainbowSkeleton } from '../Skeleton/RainbowSkeleton'
-import { ProfileDrawer } from './ProfileDrawer'
-import { Commander } from '../Commander/Commander'
+import React from "react"
+import { Box, Flex } from "@chakra-ui/react"
 
-export const Nav = props => {
+import { Commander } from "../Commander/Commander"
+import { RainbowSkeleton } from "../Skeleton/RainbowSkeleton"
+import { ProfileDrawer } from "./ProfileDrawer"
+
+export const Nav = (props) => {
   const [profileDrawerOpen, setProfileDrawerOpen] = React.useState(false)
 
   const toggleProfileDrawer = React.useCallback(() => {
@@ -14,36 +15,37 @@ export const Nav = props => {
   return (
     <>
       <Box
-        position='fixed'
-        maxW='100vw'
-        top={0}
-        left={0}
-        right={0}
+        position="fixed"
         zIndex={999}
+        top={0}
+        right={0}
+        left={0}
+        maxWidth="100vw"
       >
         <Flex
-          as='nav'
-          w='100%'
-          maxW='100%'
-          alignItems={'center'}
-          position='relative'
-          justifyContent='center'
-          flexDir={'row'}
-          py={'10px'}
-          px={2}
+          as="nav"
+          position="relative"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="row"
+          width="100%"
+          maxWidth="100%"
+          marginY={1}
+          paddingX="12px"
+          paddingY="10px"
           // bg='white'
           // boxShadow={'0px 0px 10px rgba(0,0,0,0.1)'}
         >
           <Commander></Commander>
           <RainbowSkeleton
-            ml={'auto'}
-            w='25px'
-            h='25px'
-            cursor='pointer'
+            marginLeft="auto"
+            width="25px"
+            height="25px"
+            cursor="pointer"
             onClick={toggleProfileDrawer}
-            bg={'rgba(0,0,0,0.1)'}
+            background="rgba(0,0,0,0.1)"
             sx={{}}
-            borderRadius='999px'
+            borderRadius="999px"
           ></RainbowSkeleton>
           {/* <RainbowSkeleton w='40px' ml='auto' mr={"4px"}></RainbowSkeleton>
           <RainbowSkeleton></RainbowSkeleton> */}
