5b627d46d35ddf784c46df7ea9c4484c38fec7b9
Nikolaj <lopudesigns@gmail.com>
2026-03-02 04:53:24 +1100

    test better branch deployment tagging - test .env creation staging

--- remix/vercel.sh
+++ remix/vercel.sh
@@ -11,7 +11,18 @@
 if [ -f .env.auto ]; then
 	export $(cat .env.auto | xargs)
 	echo "THINGTIME_BRANCH_NAME is $THINGTIME_BRANCH_NAME"
-	export BRANCH_NAME=$THINGTIME_BRANCH_NAME
+	# create or append to .env file with THINGTIME_BRANCH_NAME env var
+	if grep -q "THINGTIME_BRANCH_NAME" .env; then
+		# replace existing line
+		sed -i.bak "s/THINGTIME_BRANCH_NAME=.*/THINGTIME_BRANCH_NAME=\"$THINGTIME_BRANCH_NAME\"/" .env
+		rm .env.bak
+	# else if no .env create it !
+	elif [ ! -f .env ]; then
+		echo "THINGTIME_BRANCH_NAME=\"$THINGTIME_BRANCH_NAME\"" > .env
+	else
+		# append to bottom of .env
+		echo "THINGTIME_BRANCH_NAME=\"$THINGTIME_BRANCH_NAME\"" >> .env
+	fi
 else
 fi
 
