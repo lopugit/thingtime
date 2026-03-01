ff16af8c6607214c0aba19af4ce38f22315ff719
Nikolaj <lopudesigns@gmail.com>
2026-03-02 04:56:08 +1100

    test better branch deployment tagging - test .env creation better logging pt2  staging

--- remix/scripts/pre-dev.sh
+++ remix/scripts/pre-dev.sh
@@ -17,6 +17,19 @@ if [ -n "$inVercel" ]; then
 	if [ -f .env.auto ]; then
 		export $(cat .env.auto | xargs)
 		echo "THINGTIME_BRANCH_NAME is $THINGTIME_BRANCH_NAME"
+		
+		# add to or create .env and append
+		if grep -q "THINGTIME_BRANCH_NAME" .env; then
+			# replace existing line
+			sed -i.bak "s/THINGTIME_BRANCH_NAME=.*/THINGTIME_BRANCH_NAME=\"$THINGTIME_BRANCH_NAME\"/" .env
+			rm .env.bak
+		elif [ ! -f .env ]; then
+			echo "THINGTIME_BRANCH_NAME=\"$THINGTIME_BRANCH_NAME\"" > .env
+		else
+			# append to bottom of .env
+			echo "THINGTIME_BRANCH_NAME=\"$THINGTIME_BRANCH_NAME\"" >> .env
+		fi
+		
 	else
 		echo "Error: .env.auto file not found. Exiting."
 		exit 1
