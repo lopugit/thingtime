c3c86c46ec641cec9ba81c87e37662a71c33af3d
Nikolaj <lopudesigns@gmail.com>
2026-06-24 04:18:33 +1000

    Expose Vercel status in production

--- remix/app/root.tsx
+++ remix/app/root.tsx
@@ -137,10 +137,13 @@ const getDeploymentBranchName = () => {
 };
 
 const shouldShowDeploymentStatus = () => {
+  const vercelEnvironment = process.env.VERCEL_TARGET_ENV || process.env.VERCEL_ENV;
+
   return (
     process.env.NODE_ENV === 'development' ||
-    process.env.VERCEL_ENV === 'preview' ||
-    process.env.VERCEL_TARGET_ENV === 'preview'
+    vercelEnvironment === 'preview' ||
+    vercelEnvironment === 'production' ||
+    process.env.THINGTIME_SHOW_DEPLOYMENT_STATUS === 'true'
   );
 };
 
